3d680638149d3e68d2362e32e4065eb821843f2d
XPsalon6 <padilla.sebas13@gmail.com>
2025-10-31 17:00:41 -0600

    update

---
+++ Assets/WeatherHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using static System.Net.WebRequestMethods;
+using UnityEngine.Networking;
+
+public class WeatherHandler : MonoBehaviour
+{
+    [SerializeField] private float lat;
+    [SerializeField] private float lon;
+    [SerializeField] private WeatherData weatherData;
+
+    private string apiKey = "2d7c3a3aaa7c036a4477e3a3612813e1";
+
+    private string url;
+
+
+    private void Start()
+    {
+        url = $"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=hourly,daily&appid={apiKey}&units=metric";
+
+        StartCoroutine(UpdateWeather());
+    }
+
+    IEnumerator UpdateWeather()
+    {
+        UnityWebRequest request = new UnityWebRequest(url);
+        request.downloadHandler = new DownloadHandlerBuffer();
+
+        yield return request.SendWebRequest();
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(request.error);
+        }
+        else
+        {
+
+        }
+    }
+}
